c5b11d4899383a5200e9f688ce8fde599375b312
Alan Campos Izaguirre <alan@blacklion.mx>
2020-10-07 16:23:18 -0500

    se agrega carpeta lib

---
+++ spi/spiDjango/lib/authentication.py
@@ -0,0 +1,37 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import exceptions
+
+
+class CustomTokenAuthentication(TokenAuthentication):
+    """
+    Simple token based authentication.
+
+    Clients should authenticate by passing the token key in the "Authorization"
+    HTTP header, prepended with the string "Token ".  For example:
+
+        Authorization: Token 401f7ac837da42b97f613d789819ff93537bee6a
+    """
+
+    keyword = 'Token'
+    model = None
+
+    def get_model(self):
+        if self.model is not None:
+            return self.model
+        from core.models import Token
+        return Token
+
+    def authenticate_credentials(self, key):
+        model = self.get_model()
+        try:
+            token = model.objects.get(key=key)
+        except model.DoesNotExist:
+            raise exceptions.AuthenticationFailed(_('Invalid token.'))
+
+        if not token.user.is_active:
+            raise exceptions.AuthenticationFailed(
+                _('User inactive or deleted.')
+            )
+
+        return (token.user, token)
---
+++ spi/spiDjango/lib/resetPassword/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ResetpasswordConfig(AppConfig):
+    name = 'resetPassword'
